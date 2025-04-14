14d42e4db5e01c8029924b69aa83f02847cbed18
bokob <bmc0520@naver.com>
2025-04-14 22:37:12 +0900

    [FIX] 도움말 1번 보면 게임 시작 버튼 활성화

--- Assets/BMC/Scripts/UI/TitleScene/UI_HelpBtnCanvas.cs
+++ Assets/BMC/Scripts/UI/TitleScene/UI_HelpBtnCanvas.cs
@@ -4,12 +4,18 @@ using UnityEngine.UI;
 public class UI_HelpBtnCanvas : MonoBehaviour
 {
     Button _helpBtn;
+    Button _gameStartBtn;
     void Start()
     {
+        _gameStartBtn = FindAnyObjectByType<UI_GameStartBtnCanvas>().GetComponentInChildren<Button>();
+
         _helpBtn = GetComponentInChildren<Button>();
         _helpBtn.onClick.AddListener(() => { 
             Manager.Sound.PlayEffect(Define.Effect.BtnClick);
-            Manager.UI.toggleHelpPanelCanvasAction?.Invoke(); 
+            Manager.UI.toggleHelpPanelCanvasAction?.Invoke();
+
+            PlayerPrefs.SetInt("IsReadHelp", 1); // 도움말 본 적 있음
+            _gameStartBtn.interactable = true;   // 버튼 활성화
         });
     }
 }
